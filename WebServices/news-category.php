98ade8fea74dff8f20dbe5abac2694560aec330b
Zohair H <zohairhemani1@gmail.com>
2014-11-02 13:11:47 +0500

    Fix.

--- WebServices/news-category.php
+++ WebServices/news-category.php
@@ -19,7 +19,6 @@
 		$newArray['title'] = $title;
 		$newArray['file'] = $fileImage;
 		$newArray['news_id'] = $news_id;
-
 		$returnArray[] = $newArray;
 
 	}
@@ -27,7 +26,7 @@
 	echo json_encode($returnArray);
 	
 function clean($string) {
-   $string = str_replace(array("<p>","</p>","<b>","</b>","<b>","</b>","<em>","</em>","<i>","</i>","<u>","</u>"), "", $string);	
+   $string = str_replace(array("<p>","</p>","<b>","</b>","<b>","</b>","<em>","</em>","<i>","</i>","<u>","</u>","<h1>","</h1>","<h2>","</h2>","<h3>","</h3>","<li>","</li>","<ul>","</ul>","<a>","</a>"), "", $string);	
    $string = preg_replace('/[^A-Za-z0-9\-:]/', ' ', $string); // Removes special chars.
    return preg_replace('/-+/', '-', $string); // Replaces multiple hyphens with single one.
 }
--- insert.php
+++ insert.php
@@ -1,35 +1,27 @@
 <?php
-	include 'headers/connect_to_mysql.php';	
-	$news_id = $_GET['news_id'];
-	
-	
-	if($_POST)
-	{
-		//include 'headers/image_upload.php';
-	
-		$title = $_POST['title'];
-		$description = $_POST['description'];
-		$query = "UPDATE news SET time_cone = now(),  title = '$title',file = '$file', description = '$description' WHERE news_id = '$news_id'";
-		$result = mysqli_query($con,$query)
-		or die ('error2');
-	}
-	else
-	{
-		$query = "SELECT * FROM news where news_id = ${news_id}";
-		$result = mysqli_query($con,$query);	
-		$row = mysqli_fetch_assoc($result)
-		or die ('error3');
-		$title = $row['title'];
-		$description = $row['description'];
-		$file = $row['file'];
-	}
+$category = $_GET['category'];
 
+if($_POST)
+{
+	include 'headers/connect_to_mysql.php';	
+	include 'headers/image_upload.php';
+	
+    
+    
+	$title = $_POST['title'];
+	$description = $_POST['description'];
+	$query = "INSERT INTO news(title,description,file,time_cone,category)
+	VALUES ('$title','$description','$file',now(),'$category')";
+	mysqli_query($con,$query)
+	or die('error');
+}	
 ?>
+
 <!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>UFCW 5</title>
+<title>UFCW 5 -<?php echo $category; ?></title>
 <link href="css/bootstrap.css" type="text/css" rel="stylesheet">
 <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
 <link href="css/style.css" type="text/css" rel="stylesheet">
@@ -38,7 +30,6 @@
 <script src="js/jquery-1.11.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script type="text/javascript">
-
 function limitText(limitField, limitCount, limitNum) {
 	if (limitField.value.length > limitNum) {
 		limitField.value = limitField.value.substring(0, limitNum);
@@ -46,57 +37,19 @@ function limitText(limitField, limitCount, limitNum) {
 		limitCount.value = limitNum - limitField.value.length;
 	}
 }
-
-
-
 </script>
 <script type="text/javascript">
-
-function formatText(el,tag,img)
-{
-	alert(tag +"  el"+el);
-if (tag!='img')
-{
-	var textField=document.selection?document.selection.createRange().text:el.value.substring(el.selectionStart,el.selectionEnd);// IE:Moz1
-
-}
-
-else 
-	{
-
-		var textField=el;	
-		
-		alert('image tag inserted' );
-
-	}
-
-		var newText='<'+tag+'>'+textField+'</'+tag+'>';
-
-
+</script>
+<script type="text/javascript">
+function formatText(el,tag){
+var selectedText=document.selection?document.selection.createRange().text:el.value.substring(el.selectionStart,el.selectionEnd);// IE:Moz1
+var newText='<'+tag+'>'+selectedText+'</'+tag+'>';
 if(document.selection){//IE
-
-	document.selection.createRange().text=newText;
+document.selection.createRange().text=newText;
 }
 else{//Moz
-
 el.value=el.value.substring(0,el.selectionStart)+newText+el.value.substring(el.selectionEnd,el.value.length);
-
 }
-		$(document).ready( function() {
-		
-		$('#uploadit').change(function(e){
-			
-	
-	 alert(e.target.files[0].name);
-	 
-  formatText (e.target.files[0].name,'img');
-	
-});
-
-
-	});
-
-
 }
 </script>
 </head>
@@ -115,7 +68,7 @@ el.value=el.value.substring(0,el.selectionStart)+newText+el.value.substring(el.s
   </div>
   <div class="fomr">
     <form name="search-form" id="search-form" class="form-inline" role="form" enctype="multipart/form-data"
-    action="<?php 'update.php?news_id=${news_id}'?>" method="post">
+    action="insert.php?category=<?php echo $category; ?>" method="post" >
       <div class="form-group">
         <label class="sr-only" for="searchTerm">Search Term</label>
         <input type="text" class="form-control" id="searchTerm" name= "searchTerm" placeholder="Enter Search Term">
@@ -129,40 +82,40 @@ el.value=el.value.substring(0,el.selectionStart)+newText+el.value.substring(el.s
     </form>
   </div>
   <div id="box">
-    <form id="box1" class="form-horizontal" role="form" action="<?php 'update.php?news_id=${news_id}'?>" method="post" enctype="multipart/form-data">
+    <form id="box1" name="form" class="form-horizontal" role="form" action="insert.php?category=<?php echo $category; ?>" method="post" enctype="multipart/form-data" onsubmit = "return validateForm()">
       <div class="list-group">
         <p class="list-group-item disabled"> <font size="3px">Insert News</font> </p>
       </div>
-      <div class="form-group">
+      <div class="form-group"> 
+        <!--<div id="errorMessage1"></div>-->
         <label for="inputEmail3" class="col-sm-2 control-label" >News title</label>
         <div class="col-sm-10">
-          <input type="text" class="form-control" id="inputEmail3" placeholder="" name="title" value="<?php echo $title;  ?>" onKeyDown="limitText(this.form.title,this.form.countdown,200);" onKeyUp="limitText(this.form.title,this.form.countdown,200);" />
-          Maximum Character
-          <input readonly type="text" name="countdown" value="200" />
+          <input required type="text" class="form-control" id="inputEmail3" placeholder="Enter  News title" name="title" onKeyDown="limitText(this.form.title,this.form.countdown,200);" 
+onKeyUp="limitText(this.form.title,this.form.countdown,200);" />
+          Maximum characters:
+          <input readonly type="text" name="countdown" size="1"  value="200">
+          <br>
         </div>
       </div>
+      <!--<div id="errorMessage2"></div>-->
       <div class="form-group">
         <label for="inputEmail3" class="col-sm-2 control-label">Description</label>
         <div class="col-sm-10">
-          <textarea rows="5" class="form-control" id="inputEmail3" placeholder="" name="description" onKeyDown="limitText(this.form.title,this.form.countdown1,2000);" onKeyUp="limitText(this.form.title,this.form.countdown1,2000);" />
-          <?php echo $description;  ?>
-          </textarea>
+          <textarea  required  class="form-control" id="inputEmail3" placeholder="Enter Description" name="description" onKeyDown="limitText(this.form.description,this.form.countdown1,2000);" onKeyUp="limitText(this.from.description,this.form.countdown1,2000);"></textarea>
           <br>
           <input type="button" value="Bold" onClick="formatText (description,'b');" />
           <input type="button" value="Italic" onClick="formatText (description,'i');" />
           <input type="button" value="Underline" onClick="formatText (description,'u');" />
-          <input type="file" name="file" id="uploadit" onClick="formatText (description,'img');"  />
-          Maximum Character
-          <input readonly type="text" name="countdown1" value="2000" />
+          Maximum Charachter:
+          <input readonly type="text" name="countdown1" value="2000" size="2"  />
         </div>
       </div>
       <div class="form-group" id="image">
         <label for="file">News Image</label>
-        <input type="file" name="file" value =""  >
+        <input type="file" name="file" id="file" >
         <br>
       </div>
-      <div class="display"> <img src="upload/<?php echo $file;?>" id="display"/> </div>
-      <button type="submit" class="btn btn-default" id="button" formmethod="post">Post</button>
+      <button type="submit" class="btn btn-default" id="button">Post</button>
     </form>
   </div>
 </div>
